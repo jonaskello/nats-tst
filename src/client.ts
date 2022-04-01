4d873af65e01f55fc831aaad8d874a15669b5b5c
Jonas Kello <jonas.kello@gmail.com>
2022-04-01 16:27:05 +0200

    Initial commit

---
+++ src/client.ts
@@ -0,0 +1,24 @@
+import { connect, Empty, StringCodec } from "nats";
+
+async function main() {
+  // create a connection
+  //   const nc = await connect({ servers: "demo.nats.io:4222" });
+  const nc = await connect({ servers: "localhost:4222" });
+
+  // create an encoder
+  const sc = StringCodec();
+
+  // the client makes a request and receives a promise for a message
+  // by default the request times out after 1s (1000 millis) and has
+  // no payload.
+  for (let i = 0; i < 100; i++) {
+    console.time("CALL");
+    const m = await nc.request("time", Empty, { timeout: 1000 });
+    console.timeEnd("CALL");
+    console.log(`got response: ${sc.decode(m.data)}`);
+  }
+
+  await nc.close();
+}
+
+main();
---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import { connect, StringCodec, Subscription } from "nats";
+
+async function main() {
+  // create a connection
+  // const nc = await connect({ servers: "demo.nats.io" });
+  const nc = await connect({ servers: "localhost" });
+
+  // create a codec
+  const sc = StringCodec();
+
+  // this subscription listens for `time` requests and returns the current time
+  const sub = nc.subscribe("time");
+  (async (sub: Subscription) => {
+    console.log(`listening for ${sub.getSubject()} requests...`);
+    for await (const m of sub) {
+      if (m.respond(sc.encode(new Date().toISOString()))) {
+        console.info(`[time] handled #${sub.getProcessed()}`);
+      } else {
+        console.log(`[time] #${sub.getProcessed()} ignored - no reply subject`);
+      }
+    }
+    console.log(`subscription ${sub.getSubject()} drained.`);
+  })(sub);
+}
+
+main();
